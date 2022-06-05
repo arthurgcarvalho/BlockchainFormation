ddf33a59fc4a931754fe09f77282757e69f7eea6
Arthur Carvalho <39745844+arthurgcarvalho@users.noreply.github.com>
2022-06-04 22:21:08 -0400

    Fix Geth
    Fix several GETH issues

--- BlockchainFormation/blockchain_specifics/geth/Geth_Network.py
+++ BlockchainFormation/blockchain_specifics/geth/Geth_Network.py
@@ -163,10 +163,17 @@ class Geth_Network:
                 # create service file on each machine
                 # --targetgaslimit config['geth_settings']['gaslimit']--targetgaslimit '30000000'
                 # --rpcvhosts='*' --rpccorsdomain='*' --wsorigins='*' neeeded for the load balancer to work
+                #ssh_stdin, ssh_stdout, ssh_stderr = ssh_clients[index].exec_command(
+                #    f"printf '%s\\n' '[Unit]' 'Description=Ethereum go client' '[Service]' 'Type=simple' "
+                #    f"'ExecStart=/usr/bin/geth --datadir /data/gethNetwork/node/ --networkid 11 --verbosity 3 "
+                #    f"--port 30310 --targetgaslimit '30000000' --maxpeers 128 --rpc --rpcvhosts='*' --rpccorsdomain='*' --wsorigins='*' --rpcaddr 0.0.0.0  --rpcapi db,clique,miner,eth,net,web3,personal,web3,admin,txpool "
+                #    f"--nat=extip:{config['priv_ips'][index]}  --syncmode full --allow-insecure-unlock --unlock {','.join([Web3.toChecksumAddress(x) for x in account_mapping[ip]])} "
+                #    f"--password /data/gethNetwork/passwords.txt {mining_settings} --miner.etherbase {Web3.toChecksumAddress(account_mapping[ip][i])} {performance_settings}'"
+                #    f" 'StandardOutput=file:/var/log/geth.log' '[Install]' 'WantedBy=default.target' > /etc/systemd/system/geth.service")
                 ssh_stdin, ssh_stdout, ssh_stderr = ssh_clients[index].exec_command(
                     f"printf '%s\\n' '[Unit]' 'Description=Ethereum go client' '[Service]' 'Type=simple' "
                     f"'ExecStart=/usr/bin/geth --datadir /data/gethNetwork/node/ --networkid 11 --verbosity 3 "
-                    f"--port 30310 --targetgaslimit '30000000' --maxpeers 128 --rpc --rpcvhosts='*' --rpccorsdomain='*' --wsorigins='*' --rpcaddr 0.0.0.0  --rpcapi db,clique,miner,eth,net,web3,personal,web3,admin,txpool "
+                    f"--port 30310 --maxpeers 128 --http --http.vhosts='*' --http.corsdomain='*' --ws.origins='*' --http.addr 0.0.0.0  --http.api db,clique,miner,eth,net,web3,personal,web3,admin,txpool "
                     f"--nat=extip:{config['priv_ips'][index]}  --syncmode full --allow-insecure-unlock --unlock {','.join([Web3.toChecksumAddress(x) for x in account_mapping[ip]])} "
                     f"--password /data/gethNetwork/passwords.txt {mining_settings} --miner.etherbase {Web3.toChecksumAddress(account_mapping[ip][i])} {performance_settings}'"
                     f" 'StandardOutput=file:/var/log/geth.log' '[Install]' 'WantedBy=default.target' > /etc/systemd/system/geth.service")
@@ -191,10 +198,17 @@ class Geth_Network:
             else:
                 # create service file on each machine
                 # --targetgaslimit '30000000'
+                #ssh_stdin, ssh_stdout, ssh_stderr = ssh_clients[index].exec_command(
+                #    f"printf '%s\\n' '[Unit]' 'Description=Ethereum go client' '[Service]' 'Type=simple' "
+                #    f"'ExecStart=/usr/bin/geth --datadir /data/gethNetwork/node/ --networkid 11 --verbosity 3 "
+                #    f"--port 30310 --targetgaslimit '30000000' --maxpeers 128 --rpc --rpcvhosts='*' --rpccorsdomain='*' --wsorigins='*' --rpcaddr 0.0.0.0  --rpcapi db,clique,miner,eth,net,web3,personal,web3,admin,txpool "
+                #    f"--nat=extip:{config['priv_ips'][index]}  --syncmode full --allow-insecure-unlock --unlock {','.join([Web3.toChecksumAddress(x) for x in account_mapping[ip]])} "
+                #    f"--password /data/gethNetwork/passwords.txt {mining_settings} {performance_settings}' 'StandardOutput=file:/var/log/geth.log' '[Install]' "
+                #    f"'WantedBy=default.target' > /etc/systemd/system/geth.service")
                 ssh_stdin, ssh_stdout, ssh_stderr = ssh_clients[index].exec_command(
                     f"printf '%s\\n' '[Unit]' 'Description=Ethereum go client' '[Service]' 'Type=simple' "
                     f"'ExecStart=/usr/bin/geth --datadir /data/gethNetwork/node/ --networkid 11 --verbosity 3 "
-                    f"--port 30310 --targetgaslimit '30000000' --maxpeers 128 --rpc --rpcvhosts='*' --rpccorsdomain='*' --wsorigins='*' --rpcaddr 0.0.0.0  --rpcapi db,clique,miner,eth,net,web3,personal,web3,admin,txpool "
+                    f"--port 30310 --maxpeers 128 --http --http.vhosts='*' --http.corsdomain='*' --ws.origins='*' --http.addr 0.0.0.0  --http.api db,clique,miner,eth,net,web3,personal,web3,admin,txpool "
                     f"--nat=extip:{config['priv_ips'][index]}  --syncmode full --allow-insecure-unlock --unlock {','.join([Web3.toChecksumAddress(x) for x in account_mapping[ip]])} "
                     f"--password /data/gethNetwork/passwords.txt {mining_settings} {performance_settings}' 'StandardOutput=file:/var/log/geth.log' '[Install]' "
                     f"'WantedBy=default.target' > /etc/systemd/system/geth.service")
